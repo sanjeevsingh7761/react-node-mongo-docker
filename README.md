62c833322c19ebcba10400f800a1ddd85d79941c
Sanjeev Singh <sanjeev.singh@and.digital>
2020-02-19 13:23:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+#### My Template React-node-express-mongo with Docker
+
+
+##### Start
+
+```sh
+$ docker-compose build
+$ docker-compose up
+```
+### Port
+
+  - 3000 => client (React)
+  - 5000 => server (Node/Express)
+  - 27017 => mongoDB
+  - 1234 => mongoAdmin
+  
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://mongo:27017/docker-demo", {
+  useNewUrlParser: true
+});
+const connection = mongoose.connection;
+connection.once("open", () => console.log("DB Connection Success"));
+
+app.get("/", (req, res) => {
+  res.json({ "80000": "800000" });
+});
+
+const ItemSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", ItemSchema);
+app.get("/items", async (req, res) => {
+  try {
+    const newItem = { name: "New Item" };
+    const data = await Item.create(newItem);
+    res.json(data);
+  } catch (err) {
+    res.json({ err: "Error found", msg: err });
+  }
+});
+
+app.listen(5000, () => console.log("Node is running at port 5000"));
